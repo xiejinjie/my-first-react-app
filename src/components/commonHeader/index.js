1770facaa629cae9d477e7f638c5a9e7c3f996c7
xiejinjie <544482194@qq.com>
2024-07-20 08:23:32 +0800

    feat: add antd and set layout

---
+++ src/components/commonHeader/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const CommonHeader = () => {
+    return (
+        <div>
+            CommonHeader
+        </div>
+    )
+}
+
+export default CommonHeader
--- src/pages/main.js
+++ src/pages/main.js
@@ -1,13 +1,57 @@
-import React from "react"
+import { React, useState } from "react"
 import { Outlet } from "react-router-dom"
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+    UploadOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Button, Layout, Menu, theme } from 'antd';
+import CommonSider from "../components/commonSider";
+
+const { Header, Sider, Content } = Layout;
 
 const Main = () => {
+    // const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
     return (
-        <div>
-            main
-            <Outlet/>
-        </div>
-    ) 
+        <Layout className="main-container">
+            <CommonSider/>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Button
+                        type="text"
+                        // icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        // onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    Content
+                </Content>
+            </Layout>
+        </Layout>
+    );
 }
 
 export default Main
